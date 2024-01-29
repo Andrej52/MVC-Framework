4d507ad93477f37b6d565317c14394fab1effd4d
Your Andrej52 <andrejmrazik8@gmail.com>
2024-01-29 12:37:01 +0100

    edit

--- app/components/adminpanel.php
+++ app/components/adminpanel.php
@@ -13,13 +13,13 @@ NOTE: make your own HTML to suit onto your webpage
             echo '<button id="logout"  onclick="get(logout)">logout</button>';
             echo "  <div>prihlaseny ako: {$_SESSION['username']}</div>";
             switch ($_SESSION['role']) {
-                case 'member':
+                case 'admin':
                     echo ' 
                     <a href="add_topics/add_topic">add-topic</a>
                     <a href="add_topics/add_gal">add_gallery</a>
+                    <a href="add_topics/manage">management</a>
                     <a href="select">select</a>
                     ';
-                    
                     break;
                 case 'user':
                     echo ' <a href="select">select</a>';
