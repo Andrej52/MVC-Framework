3bf3d14df8a8f7c839810f79e9dc8a1b3d90b633
Andrej52 <andrejmrazik8@gmail.com>
2024-05-24 13:08:18 +0200

    updated

--- app/models/form.php
+++ app/models/form.php
@@ -5,7 +5,7 @@ class Form extends Database
     private $Colname;
     private $type = ["text","password","date","number","checkbox","file",];
     private $acceptable = ["img"=>[".png","jpg","jpeg","image/*"], "file"=>[".doc",".docx",".pdf",".xml","doc/*"]]; 
-    
+    private $jsonFilePath = 'path/to/your/';    
 
     public function __construct() {
         $this->db = new Database();
@@ -25,37 +25,43 @@ class Form extends Database
             (($type == "varchar") || ($type == "longblob") || ($type == "blob") || ($type == "tinyblob") || ($type == "mediumblob")) ? 1 : 0 // IMAGE, FILE
         ];
         var_dump($switchcases);
-        /*
-        
-        for ($i=0; $i <5 ; $i++) { 
-            if ($switchcases[$i] && !$switchcases[1] && !$switchcases[2] && !$switchcases[3] && !$switchcases[4] && !$switchcases[5]) {
-                return $type[$i]; 
-            }  
+
+        $initval = '1000000'; // initial value =>  TEXT
+        echo 'initval: '.$initval.'\n';
+
+        for ($i=0; $i < count($this->type) ; $i++) {
+            // checking each bitmap via bitshifrt right  
+            if (($initval >> $i) == $switchcases[$i]  ) {
+                var_dump($switchcases[$i]);
+                print_r($this->type[$i]);
+            }
         }
 
-        if ($switchcases[0] && !$switchcases[1] && !$switchcases[2] && !$switchcases[3] && !$switchcases[4] && !$switchcases[5]) {
-            return $type[0]; // text
-        }
-        else if (!$switchcases[0] && $switchcases[1] && !$switchcases[2] && !$switchcases[3] && !$switchcases[4] && !$switchcases[5]) {
-            return $type[1]; // password
-        }
-        else if (!$switchcases[0] && !$switchcases[1] && $switchcases[2] && !$switchcases[3] && !$switchcases[4] && !$switchcases[5]) {
-            return $type[2]; // date
-        }
-        else if (!$switchcases[0] && !$switchcases[1] && !$switchcases[2] && $switchcases[3] && !$switchcases[4] && !$switchcases[5]) {
-            return $type[3]; // number
-        }
-        else if (!$switchcases[0] && !$switchcases[1] && !$switchcases[2] && !$switchcases[3] && $switchcases[4] && !$switchcases[5]) {
-            return $type[4]; // checkbox
-        }
-        else if (!$switchcases[0] && !$switchcases[1] && !$switchcases[2] && !$switchcases[3] && !$switchcases[4] && $switchcases[5]) {
-            return $type[5]; // file
-        }
-        else
-        {
-            return null;
-        }
-        
+        /*
+            same shit ale inak        
+            if ($switchcases[0] && !$switchcases[1] && !$switchcases[2] && !$switchcases[3] && !$switchcases[4] && !$switchcases[5]) {
+                return $type[0]; // text
+            }
+            else if (!$switchcases[0] && $switchcases[1] && !$switchcases[2] && !$switchcases[3] && !$switchcases[4] && !$switchcases[5]) {
+                return $type[1]; // password
+            }
+            else if (!$switchcases[0] && !$switchcases[1] && $switchcases[2] && !$switchcases[3] && !$switchcases[4] && !$switchcases[5]) {
+                return $type[2]; // date
+            }
+            else if (!$switchcases[0] && !$switchcases[1] && !$switchcases[2] && $switchcases[3] && !$switchcases[4] && !$switchcases[5]) {
+                return $type[3]; // number
+            }
+            else if (!$switchcases[0] && !$switchcases[1] && !$switchcases[2] && !$switchcases[3] && $switchcases[4] && !$switchcases[5]) {
+                return $type[4]; // checkbox
+            }
+            else if (!$switchcases[0] && !$switchcases[1] && !$switchcases[2] && !$switchcases[3] && !$switchcases[4] && $switchcases[5]) {
+                return $type[5]; // file
+            }
+            else
+            {
+                return null;
+            }
+            
         */
 
     }
@@ -71,4 +77,31 @@ class Form extends Database
         }
         echo '</form>';
     }
+
+    // generate FORM via JSON
+    public function createFormbyJSON($filename,$formname,$controller,$method)
+    {
+        $jsonData = file_get_contents($this->jsonFilePath.$filename);
+        $form = $jsonData['formname']; 
+        echo'<form action="../app/controllers/'.$controller.'" enctype="multipart/form-data" method="'.$method.'">';
+        echo '<input type="hidden" name="table" value="'.$tableName.'">';
+        for ($i=0; $i < $colsCount; $i++) { 
+            echo '<input type="'.$form['type'].'" name="'.$form['name'].'" value="'.$form['value'].'" placeholder="'.$form['placeholder'].'">';
+        }
+        echo '</form>';
+    }
+
+    // generate FORM via XML
+    /*
+    public function createFormByXML($filename,$controller,$method)
+    {
+        echo'<form action="../app/controllers/'.$controller.'" enctype="multipart/form-data" method="'.$method.'">';
+        echo '<input type="hidden" name="table" value="'.$tableName.'">';
+        for ($i=0; $i < $colsCount; $i++) { 
+            echo '<input type="hidden" name="table" value="'.$tableName.'">';
+        }
+        echo '</form>';
+    }
+    */
+    
 }
